25db4e97c9b318dc6adef9aefe2c2450465090b8
Abderrahim Zabir <bahimzabir@gmail.com>
2023-07-31 13:23:21 +0100

    bm removed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { HomeModule } from './home/home.module';
@@ -11,7 +10,6 @@ import { HomeModule } from './home/home.module';
     ConfigModule.forRoot({ isGlobal: true, }),
     AuthModule,
     UserModule,
-    BookmarkModule,
     PrismaModule,
     HomeModule
   ],
