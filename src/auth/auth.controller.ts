2e4e93f87728fce1c23f46563eca74620473c6a5
Ayoub aitbelhaj <94859813+aaitbelh@users.noreply.github.com>
2023-08-15 08:49:27 +0000

    CR7

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,6 +11,7 @@ import {
 import { AuthService } from './auth.service';
 import { AuthDto } from 'src/dto';
 import { AuthGuard } from '@nestjs/passport';
+import { GoogleOAuthGuard } from './guard/google-oauth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -33,4 +34,27 @@ export class AuthController {
     res.redirect('http://localhost:3000/home');
     console.log({ redirected: r });
   }
+}
+@Controller("/auth/google")
+export class GoogleAuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Get('signin')
+    @UseGuards(GoogleOAuthGuard)
+    SignIn(){
+    }
+    @Get('googleRedirect')
+    @UseGuards(GoogleOAuthGuard)
+    async Google_redirect(@Req() req, @Res() res){
+        const r = await this.authService.SignIn(req);
+        res.cookie("jwt", r.token, {
+            httpOnly: true, // Ensures that the token cannot be accessed via JavaScript.
+            secure: true, // Ensures the token is only sent over HTTPS if available.
+            sameSite: "strict", // Prevents the token from being sent in cross-site requests.
+          });
+        // console.log(r.token);
+        console.log(req)
+        res.redirect("http://localhost:5173/chat");
+        // console.log({redirected: r});
+    }
 }
