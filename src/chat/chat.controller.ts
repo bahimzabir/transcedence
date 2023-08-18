7891d49a1f0ac34063a70f53a7b9e153932bcc76
Abderrahim <bahimzabir@gmail.com>
2023-08-19 00:22:47 +0100

    writing chta APIs

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -8,22 +8,5 @@ import { ChatRoomBody } from 'src/dto';
 @UseGuards(JwtGard)
 @Controller('chat')
 export class ChatController {
-    constructor(private readonly chatService: ChatService) {}
-    @Get('me')
-    getChat(@Req() req: any) {
-        // return ('chats');
-        try {
-            return this.chatService.getChats(req);
-        } catch (error) {
-            console.log("error");
-        }
-    }
-    @Post('me')
-    createChat(@Req() req: any, @Body() body: ChatRoomBody) {
-        try {
-            return this.chatService.createChat(req, body);
-        } catch (error) {
-            console.log("error");
-        }
-    }
+
 }
