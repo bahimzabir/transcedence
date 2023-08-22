0c666ad0167eb6ba392c3a098b6e4d3d891c147a
Abderrahmane Laajili <alaajili@c4r8p5.1337.ma>
2023-08-22 12:46:24 +0100

    checkout

--- src/game/game.gateway.ts
+++ src/game/game.gateway.ts
@@ -39,7 +39,7 @@ interface Room {
 
 const socketConfig = {
   cors: {
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:3000', 'http://localhost:5173'],
     credentials: true,
   },
   namespace: "game"
