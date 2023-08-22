64dae4bdc766f6a3bf1c01bc90b5c9b51a3cb6ae
Abderrahim <bahimzabir@gmail.com>
2023-08-22 02:23:45 +0100

    web socket guard tested ;-)

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -11,7 +11,6 @@ import { map } from 'rxjs/operators';
 import { Server, Socket } from 'socket.io';
 // import { JwtGard } from 'src/auth/guard';
 import {JwtStartegy} from 'src/auth/startegy'
-// import { JwtModule } from '@nestjs/jwt';
 import * as jwt from 'jsonwebtoken'
 import { WsGuard } from 'src/auth/guard';
 
@@ -23,28 +22,25 @@ const sockerConfig = {
   namespace: 'user',
 };
 
-@UseGuards(WsGuard)
 @WebSocketGateway(sockerConfig)
 export class EventsGateway {
   @WebSocketServer()
   server: Server;
-  async handleConnection(client: Socket): Promise<void> {
-    const cookies = client.handshake.headers.cookie;
-    const token = cookies ? cookies.split("=")[1] : "Cookeis not sent"
-   //let payload : any = jwt.verify(token, 'very-very-secret-hahaha')
-    console.log(token)
-    // const user = await this.jwtStrategy.validate( {sub :payload.sub, email: payload.email} );
-    console.log({Client_connected: client.id});
+  async handleConnection(client: Socket, ...args: any): Promise<void> {
+    // const cookies = client.handshake.headers.cookie;
+    // const token = cookies ? cookies.split("=")[1] : "Cookeis not sent"
+    console.log({connected: client.id});
   }
-
+  
   handleDisconnect(client: Socket): void {
     console.log(`Client disconnected: ${client.id}`);
   }
-
-
+  
+  
+  @UseGuards(WsGuard)
   @SubscribeMessage('events')
   findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
-    //console.log({ data });
+    console.log( "events is here");
     //console.log({message_from_client: data.message, id : data.id})
     return from([1, 2, 3]).pipe(
       map((item) => ({ event: 'events', data: item })),
