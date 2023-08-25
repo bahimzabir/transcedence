df4fa270574448dc05aedb2b76dac9cb952953e2
Abderrahim <bahimzabir@gmail.com>
2023-08-25 01:03:07 +0100

    the secret key of 42 is updated

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,7 +3,7 @@ import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
   async editUser(req: any, body: any) {
     try {
       const user = await this.prisma.user.update({
@@ -17,6 +17,51 @@ export class UserService {
       });
     } catch (error) {
       throw new Error('error occured while updating user');
+    }
+  }
+
+  // get user tree
+
+  async getUserTree(req: any) {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: {
+          id: req.user.id,
+        },
+        include: {
+          notifications: true,
+          friends: true,
+          outgoingFriendRequests: true,
+          incomingFriendRequests: true,
+          blockedUsers: true,
+          blockedBy: true,
+          roomUsers: {
+            select: {
+              room: {
+                select: {
+                  id: true,
+                  name: true,
+                  photo: true,
+                }
+              }
+            }
+          },
+          roomAdmins: {
+            select: {
+              room: {
+                select: {
+                  id: true,
+                  name: true,
+                  photo: true,
+                }
+              }
+            }
+          },
+        },
+      });
+      return user;
+    } catch (error) {
+      throw new Error('error occured while getting user tree');
     }
   }
 }
