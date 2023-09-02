92b256cc5466b9f8e9b9a54d3799b71b73fc7f84
Abderrahmane Laajili <alaajili@c4r7p6.1337.ma>
2023-09-02 13:55:27 +0100

    update game gateway

--- src/game/game.gateway.ts
+++ src/game/game.gateway.ts
@@ -1,187 +1,199 @@
 import { Interval } from '@nestjs/schedule';
 import { 
-  SubscribeMessage,
-  OnGatewayConnection,
-  OnGatewayDisconnect,
-  WebSocketGateway,
-  WebSocketServer,
-  MessageBody
+	SubscribeMessage,
+	OnGatewayConnection,
+	OnGatewayDisconnect,
+	WebSocketGateway,
+	WebSocketServer,
+	MessageBody,
+	ConnectedSocket
 } from '@nestjs/websockets';
 
- import * as jwt from "jsonwebtoken"
+import * as jwt from "jsonwebtoken"
 
 import { Server, Socket } from "socket.io"
+import { GameService } from './game.service';
+import { UserService } from 'src/user/user.service';
+import { Console } from 'console';
+import { RouterModule } from '@nestjs/core';
 
 
 interface BallPos {
-  x?: number;
-  y?: number;
+	x: number;
+	y: number;
 }
 
 interface GameData {
-  ballPos: BallPos;
-  leftPlayerY: number;
-  rightPlayerY: number;
-  speedX: number;
-  speedY: number;
+	ballPos: BallPos;
+	leftPlayerY: number;
+	rightPlayerY: number;
+	speedX: number;
+	speedY: number;
+	leftScore: number;
+	rightScore: number;
 }
 
-interface User {
-  id: String;
-  userSocket: Socket;
-}
+interface Player {
+	socket: Socket;
+	id: number;
+	side: string;
+};
 
 interface Room {
-  players?: Socket[];
-  roomName?: string;
-  data?: GameData;
+	roomName: string;
+	players: Player[];
+	data: GameData;
 }
 
 const socketConfig = {
-  cors: {
-    origin: ['http://localhost:3000', 'http://localhost:5173'],
-    credentials: true,
-  },
-  namespace: "game"
+	cors: {
+		origin: ['http://localhost:5173', 'http://10.14.8.7:5173', 'http://10.14.8.7:3000'],
+		credentials: true
+	},
+	namespace: 'game'
 };
 
 
 @WebSocketGateway( socketConfig )
 export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
-  @WebSocketServer()
-  server: Server;
+	// constructor(private gameService: GameService, private userService: UserService) {}
 
-  private queue: Socket[] = [];
-  private rooms: Room[] = [];
+	@WebSocketServer()
+	server: Server;
 
-  private roomNames: String[] = [];
+	private queue: Player[] = [];
+	private rooms: Room[] = [];
+
+	private roomNames: String[] = [];
+
+	async handleConnection(client: Socket) : Promise<void> {
+		const cookie = client.handshake.headers.cookie;
+		const jwtToken = cookie.split('=')[1];
+
+		const jwtPayload : any = jwt.verify(jwtToken, "8048af480a486c120bbca102e154762505e0a898");
+		const userId = jwtPayload.sub;
+
+		let player: Player = {
+			socket: client,
+			id: userId,
+			side: !(this.queue.length % 2) ? "left" : "right"
+		}
+
+		this.queue.push(player);
+
+		if (this.queue.length >= 2)
+		{
+			const players = this.queue.splice(0, 2);
+			const roomName: string = this.createNewRoom();
+
+			
+			let room: Room = {
+				roomName: roomName,
+				players: players,
+				data: {
+					ballPos: {x: 500, y:300},
+					leftPlayerY: 250,
+					rightPlayerY: 250,
+					speedX: 2,
+					speedY: 1,
+					leftScore: 0,
+					rightScore: 0
+				}
+			};
+
+			players.forEach(player => {
+				player.socket.join(roomName);
+			});
+
+			this.server.to(room.roomName).emit("join_room", {
+				data: room.data,
+				roomName: room.roomName,
+				playerOneId: players[0].id,
+				playerTwoId: players[1].id
+			});
+			this.rooms.push(room);
+		}
+	}
+
+	private createNewRoom() : string {
+		let roomName: string;
+
+		do {
+			roomName = Math.random().toString(36).substring(7);
+		} while (this.roomNames.includes(roomName));
+
+		this.roomNames.push(roomName);
+
+		return roomName;
+	}
+
+	// handle disconnection of one of the players
+	handleDisconnect(client: Socket) {
+		console.log("player lost");
+	}
+
+	@Interval(10)
+	async moveBall() {
+		for (let room of this.rooms) {
+			room.data.ballPos.x += room.data.speedX;
+			room.data.ballPos.y += room.data.speedY;
+
+			if (room.data.ballPos.y >= 595 || room.data.ballPos.y <= 5) {
+				room.data.speedY *= -1;
+			}
+			if (room.data.ballPos.x > 1000 || room.data.ballPos.x < 0) {
+				(room.data.ballPos.x > 1000) ? (room.data.leftScore += 1) : (room.data.rightScore += 1);
+				this.resetData(room);
+			}
+
+			if (room.data.ballPos.x <= 10 && (room.data.ballPos.y > room.data.leftPlayerY && room.data.ballPos.y < room.data.leftPlayerY+80)) {
+				room.data.speedX += (room.data.speedX > 0) ? 1 : (-1);
+				room.data.speedX *= -1;
+			}
+			if (room.data.ballPos.x >= 990 && (room.data.ballPos.y > room.data.rightPlayerY && room.data.ballPos.y < room.data.rightPlayerY+80)) {
+				room.data.speedX += (room.data.speedX > 0) ? 1 : (-1);
+				room.data.speedX *= -1;
+			}
+			this.server.to(room.roomName).emit("update", room.data);
+
+			if (room.data.leftScore === 9 || room.data.rightScore === 9) {
+				this.server.to(room.roomName).emit("endMatch");
+			}
+		}
+	}
+
+	private resetData(room: Room) : void  {
+		room.data.ballPos = {x: 500, y:300};
+		room.data.speedX = 2;
+		room.data.speedY = 1;
+	}
+
+	@SubscribeMessage("move")
+	async movePaddle(@MessageBody() data: any, @ConnectedSocket() client: Socket) {
+		let room = this.getRoomByName(data.roomName);
+		
+		if (room === undefined) {
+			return ;
+		}
+
+		if (client === room.players[0].socket) {
+			room.data.leftPlayerY = data.posY;
+		}
+		else if (client === room.players[1].socket) {
+			room.data.rightPlayerY = data.posY;
+		}
+		this.server.to(data.roomName).emit("update", room.data);
+	}
 
 
-
-  async handleConnection(client: Socket) : Promise<void> {
-    console.log("GOT hERE")
-    const token = client.handshake.headers.cookie;
-    
-    console.log(token);
-
-    await this.queue.push(client);
-    //client.emit("joined_queue");
-
-    // console.log(this.queue.length);
-    if (this.queue.length >= 2) {
-      const players = this.queue.splice(0, 2);
-      const roomName: string = this.createNewRoom();
-      let n = 0;
-      players.forEach(player => {
-        player.join(roomName);
-        player.emit("join_room", {side: n, roomName: roomName});
-        n++;
-      });
-
-      // console.log(roomName);
-      
-      let room: Room = {roomName: roomName,
-                        players: players,
-                        data: {
-                          ballPos: {x: 500, y:300},
-                          speedX: 2,
-                          speedY: 1,
-                          leftPlayerY: 250,
-                          rightPlayerY: 250
-                        }};
-      
-      this.rooms.push(room);
-      this.server.to(room.roomName).emit("started");
-    }
-
-
-  }
-
-
-
-  private createNewRoom() : string {
-    let roomName: string;
-    
-    do {
-      roomName = Math.random().toString(36).substring(7);
-    } while (this.roomNames.includes(roomName));
-    
-    this.roomNames.push(roomName);
-
-    return roomName;
-
-  }
-
-  // handle disconnection of one of the players
-  handleDisconnect(client: Socket) {
-    
-  }
-
-  @Interval(10)
-  async moveBall() {
-    for (let room of this.rooms) {
-      room.data.ballPos.x += room.data.speedX;
-      room.data.ballPos.y += room.data.speedY;
-
-      if (room.data.ballPos.y >= 595 || room.data.ballPos.y <= 5) {
-        room.data.speedY *= -1;
-      }
-      if (room.data.ballPos.x > 1000 || room.data.ballPos.x < 0) {
-        room.data.ballPos = {x: 500, y:300};
-        room.data.speedX *= -1;
-        room.data.speedY *= -1;
-      }
-
-      if (room.data.ballPos.x <= 10 && (room.data.ballPos.y > room.data.leftPlayerY && room.data.ballPos.y < room.data.leftPlayerY+80)) {
-        room.data.speedX *= -1;
-      }
-      if (room.data.ballPos.x >= 990 && (room.data.ballPos.y > room.data.rightPlayerY && room.data.ballPos.y < room.data.rightPlayerY+80)) {
-        room.data.speedX *= -1;
-      }
-
-      this.server.to(room.roomName).emit("update", room.data);
-    }
-
-  }
-
-
-  @SubscribeMessage("move")
-  async movePaddle(@MessageBody() data: any) {
-
-    let room = this.getRoomByName(data.roomName);
-
-    if (room === undefined) {
-      return ;
-    }
-
-    if (data.direction === "down" && data.side === "left" && room.data.leftPlayerY < 520) {
-      room.data.leftPlayerY += 10;
-    }
-    else if (data.direction === "down" && data.side === "right" && room.data.rightPlayerY < 520) {
-      room.data.rightPlayerY += 10;
-    }
-    else if (data.direction === "up" && data.side === "left" && room.data.leftPlayerY > 0) {
-      room.data.leftPlayerY -= 10;
-    }
-    else if (data.direction === "up" && data.side === "right" && room.data.rightPlayerY > 0) {
-      room.data.rightPlayerY -= 10;
-    }
-
-    this.server.to(data.roomName).emit("update",  room.data);
-
-
-  }
-
-
-  private getRoomByName(roomName: string) : Room | undefined {
-    for (let room of this.rooms) {
-      if (room.roomName === roomName) {
-        return room;
-      }
-    }
-    return undefined;
-  }
-
-  
+	private getRoomByName(roomName: string) : Room | undefined {
+		for (let room of this.rooms) {
+			if (room.roomName === roomName) {
+			  return room;
+			}
+		}
+		return undefined;
+	}
 }
+
