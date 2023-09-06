e30977727fea3b6d6a95b7358cdd9a8ed737faf1
Abderrahim <bahimzabir@gmail.com>
2023-09-06 19:10:58 +0100

    using prisma  types in user services!

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import {EventsGateway} from '../events/events.gateway';
+import { PrismaTypes } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, EventsGateway],
+  providers: [UserService, EventsGateway, PrismaTypes],
 })
 export class UserModule {}
